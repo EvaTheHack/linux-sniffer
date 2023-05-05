b4e982a2c00bed9e99586465388a8f4cf2df7b79
Roman <v-rsoloviev@youngliving.com>
2023-05-05 19:49:00 +0300

    updates

--- init.sh
+++ init.sh
@@ -3,3 +3,14 @@ sudo apt-get install -y gcc
 sudo apt-get install make
 
 make
+
+cp daemon_init /etc/init.d/daemon_sniffer
+
+OLD_DEAMON_PATH="DEAMON_PATH_VAR"
+DEAMON_PATH=$(readlink -f output/daemon_sniffer)
+
+sed -i "s|$OLD_DEAMON_PATH|$DEAMON_PATH|g" /etc/init.d/daemon_sniffer
+
+chmod +x /etc/init.d/daemon_sniffer
+
+update-rc.d daemon_sniffer defaults
